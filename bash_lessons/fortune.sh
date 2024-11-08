a77b3b21c4ae39b03ecb572f7c806cc8260b2419
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2024-11-08 12:35:06 -0300

    add bash lessons files

---
+++ bash_lessons/fortune.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Program to tell a persons fortune
+
+echo -e "\n~~ Fortune Teller ~~\n"
+
+RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+N=$(( RANDOM % 6 ))
+
+function GET_FORTUNE() {
+  if [[ $1 =~ \?$ ]]
+  then
+    echo -e "\n${RESPONSES[$N]}"
+  else
+    echo Try again. Make sure it ends with a question mark:
+  fi
+}
+
+echo Ask a yes or no question:
+read QUESTION 
+GET_FORTUNE "$QUESTION" 
